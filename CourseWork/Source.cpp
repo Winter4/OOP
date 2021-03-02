6027ea2d2563eaf089b35b9e04b6fc13eed2fd10
Winter4 <Crowl1ngNice@yandex.ru>
2021-03-02 16:41:06 +0300

    Temporate from pair

--- CourseWork/Source.cpp
+++ CourseWork/Source.cpp
@@ -1,14 +1,20 @@
 #include <SFML/Graphics.hpp>
 
+#define WINDOW_WIDTH 1600
+#define WINDOW_HEIGHT 900
+
+using namespace sf;
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Not Tetris");
-	sf::Event event;
+	sf::RenderWindow window1(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Rendzu!");
+	window1.setPosition(Vector2i(20, 30));
+	sf::Event event1;
 
-	while (window.isOpen()) 
-		while (window.pollEvent(event)) 
-			if (event.type == sf::Event::Closed) 
-				window.close();
+	while (window1.isOpen()) 
+		while (window1.pollEvent(event1)) 
+			if (event1.type == sf::Event::Closed) 
+				window1.close();
 
 	return 0;
 }
