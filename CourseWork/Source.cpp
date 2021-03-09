3360120f0552bfcfcbb0a55cc68cf67c04e504f5
Winter4 <Crowl1ngNice@yandex.ru>
2021-03-09 15:42:21 +0300

    Threads

--- CourseWork/Source.cpp
+++ CourseWork/Source.cpp
@@ -5,16 +5,73 @@
 
 using namespace sf;
 
+void firstWindow()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 300), "Rendzu!");
+	window.setPosition(Vector2i(400, 300));
+	sf::Event event;
+
+	int mover = 0;
+	float angle = 1.0;
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(Color(250, 220, 100, 0));
+
+		CircleShape circle(50);
+		circle.setFillColor(Color(230, 0, 230));
+		circle.move({ float(mover++), float(mover++) });
+		sf::sleep(seconds(0.5));
+
+		CircleShape triangle(50, 3);
+		triangle.setFillColor(Color(0, 0, 0));
+
+		Transform transform;
+		transform.rotate(angle);
+		
+		window.draw(transform, triangle);
+
+		window.draw(circle);
+		window.display();
+	}
+}
+
+void secondWindow()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 300), "Rendzu!");
+	window.setPosition(Vector2i(900, 500));
+	sf::Event event;
+
+	float angle = 1.0;
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(Color(250, 220, 100, 0));
+
+		CircleShape triangle(50, 3);
+		triangle.setFillColor(Color(0, 0, 0));
+
+		Transform transform;
+		transform.rotate(angle);
+
+		window.draw()
+
+		window.display();
+	}
+}
+
 int main()
 {
-	sf::RenderWindow window1(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Rendzu!");
-	window1.setPosition(Vector2i(20, 30));
-	sf::Event event1;
+	Thread firstWindowThread(&firstWindow);
+	firstWindowThread.launch();
 
-	while (window1.isOpen()) 
-		while (window1.pollEvent(event1)) 
-			if (event1.type == sf::Event::Closed) 
-				window1.close();
-
+	Thread secondWindowThread(&secondWindow);
+	secondWindowThread.launch();
 	return 0;
 }
